0a6e214465a4329150a60bca6ff9b89278f0d682
mugund10 <bjmugundhan@gmail.com>
2024-07-09 16:06:40 +0530

    gitsolver

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,10 +6,17 @@ import (
 	"log"
 	"net"
 
+	"encoding/json"
+
+	"github.com/golang/protobuf/ptypes/any"
+	"github.com/golang/protobuf/ptypes/wrappers"
 	"github.com/mugund10/grpcuserservice/internal/db"
 	"github.com/mugund10/grpcuserservice/pb"
+	"github.com/mugund10/grpcuserservice/pkg/search"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/anypb"
 )
 
 func main() {
@@ -47,11 +54,12 @@ type Userser struct {
 	pb.UnimplementedUserserviceServer
 }
 
+// fetches user details based on a user id
 func (u *Userser) Fetchuser(ctx context.Context, req *pb.Requestid) (*pb.User, error) {
 
 	people := db.UsersList
 	resp := pb.User{}
-
+	//
 	for _, value := range people {
 		if value.Id == req.Id {
 			resp = pb.User{
@@ -64,7 +72,7 @@ func (u *Userser) Fetchuser(ctx context.Context, req *pb.Requestid) (*pb.User, e
 			}
 		}
 	}
-
+	//if the requested id is not prersent on the database then its returns a error
 	if req.Id != resp.Id {
 		return nil, fmt.Errorf("user %d is not found", req.Id)
 	}
@@ -72,13 +80,14 @@ func (u *Userser) Fetchuser(ctx context.Context, req *pb.Requestid) (*pb.User, e
 	return &resp, nil
 }
 
+// retrieves a list of user details based on a list of user id's
 func (u *Userser) Fetchusers(ctx context.Context, req *pb.Requestids) (*pb.Users, error) {
 	people := db.UsersList
 	usersli := []*pb.User{}
 
 	for _, value1 := range req.Id {
 		for _, value2 := range people {
-			
+
 			if value1.Id == value2.Id {
 				usersli = append(usersli, &pb.User{
 					Id:      value2.Id,
@@ -94,9 +103,55 @@ func (u *Userser) Fetchusers(ctx context.Context, req *pb.Requestids) (*pb.Users
 	}
 
 	resp := pb.Users{
-        User: usersli,
-    }
+		User: usersli,
+	}
 
-    return &resp, nil
+	return &resp, nil
 
+}
+
+func (u *Userser) Search(ctx context.Context, req *pb.Criteria) (*pb.Users, error) {
+
+	users := db.UsersList
+	search := search.Details{Users: users}
+	usersli := []*pb.User{}
+
+	field := req.Field
+	value , err := ConvertAnyToInterface(req.Value)
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+
+	muser := search.ByCriteria(field, value)
+	fmt.Printf("users with criteria %s = %v:\n", field, value)
+	for _, user := range muser {
+
+		usersli = append(usersli, &pb.User{
+			Id:      user.Id,
+			Fname:   user.Fname,
+			City:    user.City,
+			Phone:   user.Phone,
+			Height:  user.Height,
+			Married: user.Married,
+		})
+	}
+
+	resp := pb.Users{
+		User: usersli,
+	}
+	return &resp, nil
+}
+
+func ConvertAnyToInterface(anyValue *any.Any) (interface{}, error) {
+	var value interface{}
+	bytesValue := &wrappers.BytesValue{}
+	err := anypb.UnmarshalTo(anyValue, bytesValue, proto.UnmarshalOptions{})
+	if err != nil {
+		return value, err
+	}
+	uErr := json.Unmarshal(bytesValue.Value, &value)
+	if uErr != nil {
+		return value, uErr
+	}
+	return value, nil
 }
--- cmd/server/server_test.go
+++ cmd/server/server_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/mugund10/grpcuserservice/pb"
+	"github.com/mugund10/grpcuserservice/pkg/anysolver"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -18,33 +19,28 @@ func Test(t *testing.T) {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	
-
 
 	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(creds))
-		errcheck(err)
+	errcheck(err)
 	defer conn.Close()
 
 	log.Printf("connected to %s", addr)
 
-	c := pb.NewUserserviceClient(conn)
+	client := pb.NewUserserviceClient(conn)
 
 	req := pb.Requestid{
 		Id: 2,
 	}
 
-	resp, err := c.Fetchuser(ctx, &req)
+	resp, err := client.Fetchuser(ctx, &req)
 	errcheck(err)
 	fmt.Println(resp)
 
-
-
-
-one := pb.Requestid{Id: 3,}
-two := pb.Requestid{Id: 7,}
-three := pb.Requestid{Id: 9,}
-four := pb.Requestid{Id: 12,}
-multipleids := []*pb.Requestid{&one,&two,&three,&four}
+	one := pb.Requestid{Id: 3}
+	two := pb.Requestid{Id: 7}
+	three := pb.Requestid{Id: 9}
+	four := pb.Requestid{Id: 12}
+	multipleids := []*pb.Requestid{&one, &two, &three, &four}
 
 	req2 := pb.Requestids{
 		Id: multipleids,
@@ -53,10 +49,51 @@ multipleids := []*pb.Requestid{&one,&two,&three,&four}
 	ctx2, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	resp2, err := c.Fetchusers(ctx2, &req2)
+	resp2, err := client.Fetchusers(ctx2, &req2)
 	errcheck(err)
 	fmt.Println(resp2)
 
+	value, err := ConvertInterfaceToAny("MUMBAI")
+	errcheck(err)
+
+	log.Println(value.TypeUrl, string(value.Value))
+
+	searchRequest := &pb.Criteria{
+		Field: "city",
+		Value: value,
+	}
+	log.Println(searchRequest)
+
+	users, err := client.Search(context.Background(), searchRequest)
+	if err != nil {
+		log.Fatalf("Search request failed: %v", err)
+	}
+
+	// Process the response from the server.
+	for _, user := range users.User {
+		log.Printf("User ID: %d, First Name: %s, City: %s", user.Id, user.Fname, user.City)
+	}
+
+	value1, err := anysolver.ConvertInterfaceToAny(true)
+	errcheck(err)
+
+	log.Println(value1.TypeUrl, string(value1.Value))
+
+	searchRequest1 := &pb.Criteria{
+		Field: "married",
+		Value: value1, // Set ny as the value for the Any field
+	}
+	log.Println(searchRequest1)
+
+	users1, err := client.Search(context.Background(), searchRequest1)
+	if err != nil {
+		log.Fatalf("Search request failed: %v", err)
+	}
+
+	// Process the response from the server.
+	for _, user := range users1.User {
+		log.Printf("User ID: %d, First Name: %s, City: %s", user.Id, user.Fname, user.City)
+	}
 
 }
 
